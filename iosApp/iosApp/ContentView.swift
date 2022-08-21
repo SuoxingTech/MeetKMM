4f4aea8de966bd02fa73611a3f3016696738f960
Yifan Wong <wifi@iwoke.work>
2022-08-21 20:14:06 +0800

    feat: memo demo for iOS

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,10 +3,32 @@ import Combine
 import shared
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ObservableViewModel(HelloViewModel())
+    @ObservedObject var viewModel = ObservableViewModel(MemoViewModel())
+    @State var input: String = ""
+    
+    init() {
+        viewModel.actor.start()
+    }
 
 	var body: some View {
-        Text(viewModel.uiState as String)
+        if viewModel.uiState.firstBook != nil {
+            Text(viewModel.uiState.firstBook?.title ?? "No Title")
+            Text((viewModel.uiState.firstBook?.memos
+                .map { m in
+                    (m as! Memo).content
+                }
+                .joined(separator: "\n")) ?? "no memos")
+            TextField("input memo", text: $input)
+            Button("ADD MEMO", action: {
+                viewModel.actor.createMemo(content: input, bookName: viewModel.uiState.firstBook?.title ?? "")
+                input = ""
+            })
+        } else {
+            Text("There is no memo books yet.")
+            Button("CREATE ONE", action: {
+                viewModel.actor.createMemoBook()
+            })
+        }
 	}
 }
 
